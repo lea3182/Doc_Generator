f99aef5cecc6e2205521c663319853116f23940f
lea3182 <lkim3182@gmail.com>
2015-09-04 09:45:21 -0700

    Edit migration

--- db/migrate/20150904164015_create_documents.rb
+++ db/migrate/20150904164015_create_documents.rb
@@ -1,6 +1,8 @@
 class CreateDocuments < ActiveRecord::Migration
   def change
     create_table :documents do |t|
+      t.references :user, index: true
+      t.string 
 
       t.timestamps null: false
     end
