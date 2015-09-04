22c80980b6cc4cd70e0ada0394b6621f3d35242d
lea3182 <lkim3182@gmail.com>
2015-09-04 12:01:59 -0700

    Test document create functionality

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,11 +1,14 @@
 class DocumentsController < ApplicationController
-  
+
   def new
     @document = Document.new
     p params
   end
 
   def create
-
+   @user = current_user
+    # @document.create(title: params[:title], user_id: params[:user_id])
+    @document = @user.documents.create(params[:document].permit(:title, :text))
+    redirect_to user_path(@user)
   end
 end
