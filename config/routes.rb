3f32b6ca3c3aa38cc56f06b440b0825f4afa5e8b
lea3182 <lkim3182@gmail.com>
2015-09-04 08:28:48 -0700

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
-  resource :users
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
