fb7a79154fca10e741e4a1be5bc3759063e0caac
lea3182 <lkim3182@gmail.com>
2015-09-05 13:28:20 -0700

    Edit PDF template

--- app/Pdfs/doc_pdf.rb
+++ app/Pdfs/doc_pdf.rb
@@ -2,6 +2,17 @@ class DocPdf < Prawn::Document
   def initialize(doc)
     super()
     @doc = doc
-    text "#{@doc.title}"
+    heading
+    doc_info
   end 
+
+  def heading
+    text "#{@doc.user.first_name}'s #{@doc.title}"
+  end
+
+  def doc_info
+    text "Loan Amount: #{@doc.amount}"
+    text "Down Payment: #{@doc.down_payment}"
+    text "Interest Rate: #{@doc.interest_rate}"
+  end
 end
