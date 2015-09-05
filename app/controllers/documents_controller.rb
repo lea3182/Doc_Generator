47ed148eee42c9f0b3c9b348a5bf23c7bac923c9
lea3182 <lkim3182@gmail.com>
2015-09-05 13:10:29 -0700

    Prawn functionality

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,13 +2,32 @@ class DocumentsController < ApplicationController
 
   def new
     @document = Document.new
-    p params
   end
 
   def create
-   @user = current_user
-    # @document.create(title: params[:title], user_id: params[:user_id])
-    @document = @user.documents.create(params[:document].permit(:title, :text))
+    @user = current_user
+    @document = @user.documents.create(doc_params)
     redirect_to user_path(@user)
   end
+
+  def show
+    @document = Document.find(params[:id])
+    
+    respond_to do |format|
+      format.html
+      format.pdf do 
+        pdf = DocPdf.new(@document)
+
+        send_data pdf.render, type: 'application/pdf', 
+                              disposition: "inline"
+      end
+    end
+  end
+
+ private
+
+  def doc_params
+    params.require(:document).permit(:title, :amount, :interest_rate, :down_payment)
+  end
+
 end
