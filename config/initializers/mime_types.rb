f04a100ac9432453d2895813f2c3231c633eb097
lea3182 <lkim3182@gmail.com>
2015-09-10 11:40:05 -0700

    Add comment

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,5 +2,5 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-# Mime::Type.register "application/pdf", :pdf
+# warning: already initialized constant Mime::PDF errors occur without unless statement
 Mime::Type.register "application/pdf", :pdf unless Mime::Type.lookup_by_extension(:pdf)
