13dc45f799244b62dd5f7e72be6004dd395ee2cb
lea3182 <lkim3182@gmail.com>
2015-09-11 15:40:38 -0700

    BUG => Gmail thinks my email is spam

--- app/mailers/doc_mailer.rb
+++ app/mailers/doc_mailer.rb
@@ -7,3 +7,7 @@ class DocMailer < ApplicationMailer
     mail ({to: @user.email, subject: "New Document Confirmation"})  # this syntax works
   end
 end
+
+
+# Gmail is warning me that these emails might be spam, need to fix.
+# http://blog.mailgun.com/tips-tricks-avoiding-gmail-spam-filtering-when-using-ruby-on-rails-action-mailer/
