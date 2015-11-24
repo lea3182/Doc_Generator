f7db5f4b98caa7a130287a11f8bcea1ba55078ff
lea3182 <lkim3182@gmail.com>
2015-11-24 14:25:04 -0800

    Switching production email config to Mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
   # AWS config variables
   config.paperclip_defaults = {
   storage: :s3,
@@ -94,13 +93,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-  address: "stmp.gmail.com",
+  address: "stmp.mandrillapp.com",
   port: 587,
-  domain: 'doc-generator.herokuapp.com',
-  authentication: 'plain',
+  domain: 'doc-generator.herokuapp.com',  # is it heroku or herokuapp
+  authentication: 'login',
   enable_starttls_auto: true,
-  user_name: ENV['GMAIL_USERNAME'],
-  password: ENV['GMAIL_PASSWORD']
+  user_name: ENV['MANDRILL_USERNAME'],
+  password: ENV['MANDRILL_PASSWORD']
 
 }
 end
