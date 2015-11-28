a1b437b07f7c799b171bf1ac554375338f2f516d
lea3182 <lkim3182@gmail.com>
2015-11-28 01:41:59 -0800

    Heroku log: Errno::ENOENT (No such file or directory @ rb_sysopen - /app/app/pdfs/45.pdf). Change file path to test in production

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -9,16 +9,22 @@ class DocumentsController < ApplicationController
     @document = @user.documents.new(doc_params) 
     @document.save
     pdf = DocPdf.new(@document) 
-    pdf.render_file("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
+    pdf.render_file("app/pdfs/#{@document.id}.pdf")
     @file_name = pdf.file_name      
     @document.doc_pdf_file_name = @file_name
   
     if @document.save
+      # s3 = Aws::S3::Resource.new(
+      #      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+      #      region: 'us-west-1')
+      # s3.bucket(ENV['S3_BUCKET']).object(@file_name).upload_file("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
+      # File.delete("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
+
       s3 = Aws::S3::Resource.new(
            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
            region: 'us-west-1')
-      s3.bucket(ENV['S3_PRODUCTION_BUCKET']).object(@file_name).upload_file("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
-      File.delete("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
+      s3.bucket(ENV['S3_PRODUCTION_BUCKET']).object(@file_name).upload_file("app/pdfs/#{@document.id}.pdf")
+      File.delete("app/pdfs/#{@document.id}.pdf")
 
       DocMailer.doc_confirmation(@user, @document).deliver_now
       redirect_to user_path(@user, @document), notice: 'Document was successfully created. Email confirmation sent'
