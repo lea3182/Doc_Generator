25510a9d598fb73eac375d71c84efec2e0ce5ab2
lea3182 <lkim3182@gmail.com>
2015-12-09 11:43:04 -0800

    Delete production bucket from configurations. document.doc_pdf.url is registered with development s3 bucket

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -18,7 +18,7 @@ class DocumentsController < ApplicationController
       s3 = Aws::S3::Resource.new(
            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
            region: 'us-west-1')
-      s3.bucket(ENV['S3_PRODUCTION_BUCKET']).object(@file_name).upload_file("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
+      s3.bucket(ENV['S3_BUCKET']).object(@file_name).upload_file("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
       File.delete("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
       # s3 = Aws::S3::Resource.new(region:'us-west-2')
       # obj = s3.bucket('bucket-name').object('key')
