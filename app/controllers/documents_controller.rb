86c74bbf0bbb51039f9f9c4d10f99a51a439a76c
lea3182 <lkim3182@gmail.com>
2015-12-12 14:44:48 -0800

    push to heroku

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -22,7 +22,7 @@ class DocumentsController < ApplicationController
       s3.bucket(ENV['S3_BUCKET']).object(@file_name).upload_file("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
       File.delete("#{Rails.root}/app/pdfs/#{@document.id}.pdf")
 
-      # DocMailer.doc_confirmation(@user, @document).deliver_now
+      DocMailer.doc_confirmation(@user, @document).deliver_now
       redirect_to user_path(@user, @document), notice: 'Document was successfully created. Email confirmation sent'
     else
       redirect_to root_path, alert: "Document did not save. Please resubmit"
