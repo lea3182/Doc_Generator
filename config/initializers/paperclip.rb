11d0e09c4a780d91d29449b25b7411a4e393a950
lea3182 <lkim3182@gmail.com>
2015-12-15 12:03:28 -0800

    Add comments

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,13 +1,5 @@
-# Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-# Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-# Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-1.amazonaws.com'
-
-# These options were originally in model. 
-  # url: "/documents/:id/:style/:basename.:extension", #where to retrieve the files
-  # path: ":rails_root/public/doc_pdf/:id/:style/:basename.:extension" #where to save the file 
-  # :url => "/assets/providers/:id/:style/:basename.:extension",
-  # :path => ":rails_root/public/assets/providers/:id/:style/:basename.:extension"
-
-  Paperclip.interpolates :user_id do |attachment, style|
-    attachment.instance.user_id
-  end
+# Paperclip only allows a limited set of values to be used in the path: filename, timestamp, rails_env, class, basename, extension, id, fingerprint, id_partition, attachment, and style. 
+# This allows access to user_id in paperclip path
+Paperclip.interpolates :user_id do |attachment, style|
+  attachment.instance.user_id 
+end
