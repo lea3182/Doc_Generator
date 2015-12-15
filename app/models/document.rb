c4016e12f263c1828f24582df13d91a4a4b12bdb
lea3182 <lkim3182@gmail.com>
2015-12-15 12:04:53 -0800

    Remove path

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,9 +1,6 @@
 class Document < ActiveRecord::Base
   belongs_to :user
-
-  has_attached_file :doc_pdf,
-  :path => "users/:user_id/:class/:filename"
-
+  has_attached_file :doc_pdf
   validates_attachment_content_type :doc_pdf, content_type: "application/pdf"
 end
 
